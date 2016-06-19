8a63004d0048a467e9a2a7e377fbb6d9ef12ada2
densmersh <denmail.by@mail.ru>
2016-06-19 14:16:17 +0300

    commit1

---
+++ EasyEngineer/Controllers/NewsController.cs
@@ -0,0 +1,89 @@
+﻿using EasyEngineer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using System.Data.Entity;
+
+namespace EasyEngineer.Controllers
+{
+    public class NewsController : Controller
+    {
+        //
+        // GET: /News/
+        public ActionResult Index(News news, string username)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+
+            if (ModelState.IsValid)
+            {
+                
+                news.Author = Author();
+
+                news.Date_pub = DateTime.Now;
+                db.Newss.Add(news);
+                db.SaveChanges();
+
+                return RedirectToAction("Show");
+            }
+            return View();
+        }
+
+        public ViewResult Show()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            var news = db.Newss;
+            return View(news);
+        }
+
+        public ActionResult Delete(int ID)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            News news = db.Newss.Find(ID);
+            db.Newss.Remove(news);
+            db.SaveChanges();
+            return RedirectToAction("Show");
+        }
+        public ViewResult CurrentShow(int ID)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            News news = db.Newss.FirstOrDefault(x => x.Id == ID);
+            return View(news);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            News news = db.Newss.FirstOrDefault(x => x.Id == ID);
+            return View(news);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(News news,string username)
+        {
+            if (ModelState.IsValid)
+            {
+                news.Author = Author();  
+                ApplicationDbContext db = new ApplicationDbContext();
+                db.Entry(news).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Show");
+            }
+            return View(news);
+        }
+
+        public string Author()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            string currentUserId = User.Identity.GetUserId();
+            ApplicationUser user = db.Users.FirstOrDefault(x => x.Id == currentUserId);
+            string username = user.UserName;
+
+            return(username);
+
+        }
+	}
+}
---
+++ EasyEngineer/Models/IdentityModels.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace EasyEngineer.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+    }
+
+    public class News
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Название топика")]
+        [MaxLength(50, ErrorMessage = "Название слишком длинное")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Как это работает?")]
+        [MaxLength(10000, ErrorMessage = "Краткость - сестра таланта!")]
+        public string Text { get; set; }
+
+        public string Author { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy H:mm:ss}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime? Date_pub { get; set; }
+        [Required]
+        [Display(Name = "Теги")]
+        [MaxLength(10, ErrorMessage = "Краткость - сестра таланта!")]
+
+        public int Like { get; set; }
+        public int Dislike { get; set; }
+        public string Tag { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+       
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<News> Newss { get; set; }
+    }
+}
